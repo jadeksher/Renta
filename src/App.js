c8ddc0ffc276e0befd2b3d8ad7c425d4b00e0ca6
Jad <jadeksher@hotmail.com>
2023-01-22 16:19:22 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.scss';
-import Header from './components/Header';
+import "./App.scss";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
-        <Header />
+      <>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/car-removebg-preview.png";
+import "./Header.scss";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav id="nav">
+      <div className="left">
+        <div className="logo">
+          <img src={Logo} alt="car" />
+          <h2>Renta</h2>
+        </div>
+      </div>
+      <div className="right">
+        <ul>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "navActive" : null)}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/services"
+            className={({ isActive }) => (isActive ? "navActive" : null)}
+          >
+            Services
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "navActive" : null)}
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) => (isActive ? "navActive" : null)}
+          >
+            Contact
+          </NavLink>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
